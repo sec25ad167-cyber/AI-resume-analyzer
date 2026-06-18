1f9ad99515c9cb4e3587900e06afc72347355d70
Sowandariya T V <sec25ad167@sairamtap.edu.in>
2026-06-18 13:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to AI Resume Analyzer!"
+    return render_template("upload.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
