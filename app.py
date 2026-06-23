8d7ad819d3095318f2c68427892c031b0201918b
Sowandariya T V <sec25ad167@sairamtap.edu.in>
2026-06-23 22:30:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def upload_resume():
+
+    file = request.files["resume"]
+
+    if file:
+
+        score = 85
+
+        suggestions = [
+            "Add more projects",
+            "Include GitHub profile",
+            "Add certifications",
+            "Add internship experience"
+        ]
+
+        return render_template(
+            "result.html",
+            score=score,
+            suggestions=suggestions
+        )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
